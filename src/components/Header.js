d25aff194b3b2c3f80210f7c660807e45294146a
Iago Viotti <iago.viotti@gmail.com>
2023-04-05 14:59:28 -0300

    header, feat. carousel, primeiras configurações

---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import { HashLink } from "react-router-hash-link";
+import logo from '../css/img/VekoTec-colored-logo.png';
+
+export default function Header() {
+  return (
+    <div
+      id="header"
+    >
+      <img
+        className="h-8"
+        src={logo}
+        alt="VekotekLogo"
+      />
+      <div
+        id='header-links'
+      >
+        <HashLink
+          className='header-link'
+          smooth to={'/page#home'}>
+          Home
+        </HashLink>
+        <HashLink
+          className='header-link'
+          smooth to={'/page#serviços'}>
+          Serviços
+        </HashLink>
+        <HashLink
+          className='header-link'
+          smooth to={'/page#sobre'}>
+          Sobre
+        </HashLink>
+        <HashLink
+          className='header-link'
+          smooth to={'/page#contato'}>
+          Contato
+        </HashLink>
+      </div>
+    </div>
+  )
+}
