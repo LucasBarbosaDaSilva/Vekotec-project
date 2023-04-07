3c7979d38575d829c4a9f65d7b7deb0d167855e2
Iago Viotti <iago.viotti@gmail.com>
2023-04-07 16:20:07 -0300

    new cool header, cellphone and ipad mini medias, so far

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,59 @@
+import { useState } from "react";
 import { HashLink } from "react-router-hash-link";
 import logo from '../css/img/VekoTec-colored-logo.png';
+import { pushDown } from "../css/img/svg";
 
 export default function Header() {
+  const [header, setHeader] = useState('hidden');
+  const [headerActive, setHeaderActive] = useState('headerInactive')
+
+  const handleMenu = () => {
+    setHeaderActive(headerActive === 'headerActive' ? 'headerInactive' : 'headerActive');
+    setHeader(header === 'hidden' ? 'display' : 'hidden');
+  }
   return (
     <div
       id="header"
+      className={headerActive}
     >
-      <img
-        src={logo}
-        alt="VekotekLogo"
-      />
+      <div
+        id='header-imgs'
+      >
+        <img
+          id='logo'
+          src={logo}
+          alt="VekotekLogo"
+        />
+        <button
+          onClick={handleMenu}
+        >
+          <img
+            id='pushdown'
+            src={pushDown}
+            alt="dropdown menu"
+          />
+        </button>
+      </div>
       <div
         id='header-links'
       >
         <HashLink
-          className='header-link'
+          className={header}
           smooth to={'/page#home'}>
           Home
         </HashLink>
         <HashLink
-          className='header-link'
+          className={header}
           smooth to={'/page#serviços'}>
           Serviços
         </HashLink>
         <HashLink
-          className='header-link'
+          className={header}
           smooth to={'/page#sobre'}>
           Sobre
         </HashLink>
         <HashLink
-          className='header-link'
+          className={header}
           smooth to={'/page#contato'}>
           Contato
         </HashLink>
