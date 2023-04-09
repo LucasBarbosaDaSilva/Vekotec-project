e213e90d6d3733975d98c7f39c98c8012bc4367a
LucasBarbosaDaSilva <l.barbosatkd@gmail.com>
2023-04-09 19:56:56 -0300

    Criando css video container, acessando Vlibras

--- src/components/VideoClip.js
+++ src/components/VideoClip.js
@@ -5,12 +5,20 @@ const VideoClip = () => {
 
   return (
     <div className="video-container">
+      <div className="video-background" />
+      <div className="embelishment-div-video" />
     <iframe
       src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
       title="YouTube video player"
       allow="autoplay"
       allowFullScreen
     />
+    <div className="missão-container">
+      <h1 className="missao-title">Missão</h1>
+      <p className="paragraph-title">
+      Transformamos problemas globais em soluções sustentáveis, preservando o ambiente para as futuras gerações.
+      </p>
+    </div>
   </div>
   );
 };
--- src/components/WhatsApp.js
+++ src/components/WhatsApp.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import VLibras from '@djpfs/react-vlibras'
 
 
 function WhatsApp() {
   return (
+    <div id="whats-container">
     <a 
       href="https://api.whatsapp.com/send?phone=32988080152"
       className="whatsapp-link"
@@ -13,6 +15,8 @@ function WhatsApp() {
       src="https://imagepng.org/wp-content/uploads/2017/08/WhatsApp-icone-3.png"
       alt="WhatsApp" />
     </a>
+    <VLibras forceOnload={true} />
+    </div>
   );
 }
 
