49263549a854206a481320dca1816c5758acf96d
Iago Viotti <iago.viotti@gmail.com>
2023-06-27 16:27:43 -0300

    values

--- src/components/Values.js
+++ src/components/Values.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import { ongValues  } from '../css/img/svg/onu-svgs';
+import { ongValues } from '../css/img/svg/onu-svgs';
 import '../Values.css';
 
 function Values() {
-
-  const divStyle = {
-    backgroundColor: 'rgb(241,105,77)',
-    width: '100%'
-  };
-
   return (
-    <div style={divStyle} className="background-image">
-      <div className="App">
-        <h1 className="value-title">Valores</h1>
-        <div className="value-container">
-          {ongValues.map(value =>
+    <div className='values-container'>
+      <h1 className="value-title">Valores</h1>
+      <div className="value-container">
+        {ongValues.map(value =>
           <div className="value-card">
             <img src={value.image} alt="logo" height={10} />
           </div>)}
-        </div>
-        <div className="embeleshment" />
       </div>
     </div>
   );
