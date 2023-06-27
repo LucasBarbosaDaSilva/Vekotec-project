da63495f7f5e37d1c3a99f397ae2ffd2be328cc5
Iago Viotti <iago.viotti@gmail.com>
2023-06-27 19:03:53 -0300

    last minor changes

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import { useState } from 'react';
 import '../Clients.css';
 
 function Clients() {
+  // const [isShaking, setIsShaking] = useState(false);
+
   return (
     <div className="clients-container">
-      <div className="">
+      <div className="left-container">
       <h1 className="client-title">Cadastro de Clientes</h1>
       <p className="cliente-p">Essas informações serão utilizadas para criação de contratos, peças técnicas, e para o contato com o cliente.</p>
       </div>
-      <div>
+      <div className="right-container">
       <a href="https://docs.google.com/forms/d/e/1FAIpQLSd5DOzvcZHjwvPV79gAlcFiFiNfpvKDquBPlK55pu0tgNVUiQ/viewform" target="_blank" rel="noopener noreferrer">
-        <button className="button-client">Clique aqui para se cadastrar!</button>
+        <button
+        className={`button-client`}
+        >Clique aqui para se cadastrar!</button>
       </a>
       </div>
     </div>
