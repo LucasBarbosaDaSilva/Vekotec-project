77deed09cf2df93f8475226729dc591160a89ce7
LucasBarbosaDaSilva <l.barbosatkd@gmail.com>
2023-07-24 14:37:43 -0300

    Alteração na cor da letra do carousel principal

--- src/css/img/logo-carrousel/index.js
+++ src/css/img/logo-carrousel/index.js
@@ -1,29 +1,29 @@
-import logoESG from './logo-esg.png';
-import logoCAR from './logo-car.png';
-import logoINCRA from './logo-incra.png';
-import { back1, back2, back3 } from '../../img/backgrounds';
+import logoESG from "./logo-esg.png";
+import logoCAR from "./logo-car.png";
+import logoINCRA from "./logo-incra.png";
+import { back1, back2, back3 } from "../../img/backgrounds";
 
 const carrousel = [
   {
-    title: `eorreferenciamento certificado pelo`,
+    title: `eorreferenciamento Certificado pelo`,
     image: back1,
     logo: logoINCRA,
-    classe:'incra',
-    classeTitle:'incra-title',
+    classe: "incra",
+    classeTitle: "incra-title",
   },
   {
-    title: ' cadastro de Imóveis Rurais',
+    title: " Cadastro de Imóveis Rurais",
     image: back2,
     logo: logoCAR,
-    classe:'car',
-    classeTitle:'car-title',
+    classe: "car",
+    classeTitle: "car-title",
   },
   {
-    title: ' é com a Vekotec',
+    title: " é com a Vekotec",
     image: back3,
     logo: logoESG,
-    classe:'esg',
-    classeTitle:'esg-title',
+    classe: "esg",
+    classeTitle: "esg-title",
   },
 ];
 
