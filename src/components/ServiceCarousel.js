f84bb50ae5a03ce75ebeade4ff5cd1de543ab1b2
LucasBarbosaDaSilva <l.barbosatkd@gmail.com>
2023-07-31 14:33:14 -0300

    Mudanço carousel service para cards, usando Grid

--- src/components/ServiceCarousel.js
+++ src/components/ServiceCarousel.js
@@ -1,77 +1,30 @@
-import React, { Component } from "react";
-import Slider from "react-slick";
-import { servicesArray } from '../css/img/svg/service-carousel'
-import '../css/service-carousel.css'
+import React from "react";
+import { servicesArray } from "../css/img/svg/service-carousel";
+import "../css/service-carousel.css";
 
-export default class ServiceCarousel extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      nav1: null,
-      nav2: null
-    };
-  }
-
-  componentDidMount() {
-    this.setState({
-      nav1: this.slider1,
-      nav2: this.slider2
-    });
-  }
-
-  render() {
-    return (
-      <div
-        id='services'
-      >
-        <h1 >CONHEÇA NOSSOS SERVIÇOS</h1>
-        <Slider
-          asNavFor={this.state.nav1}
-          ref={slider => (this.slider2 = slider)}
-          slidesToShow={6}
-          swipeToSlide={true}
-          focusOnSelect={true}
-          centerMode={true}
-        >
-          {servicesArray.map(service => (
-            <div
-            key={service.name}
-              className='service-icon-div'
-            >
-              <img
-                className='service-icon'
-                src={service.icon}
-                alt={`${service.name} icon`}
-              />
-            </div>
-          ))}
-        </Slider>
-        <Slider
-          asNavFor={this.state.nav2}
-          ref={slider => (this.slider1 = slider)}
-        >
-          {servicesArray.map(service => (
-            <div
-              key={service.name}
-              className='service-div'
-            >
-              {service.content.map(content => (
-                <div
-                  key={content.title}
-                  className='service-content-div'
-                >
-                  <h2>
-                    {content.title}
-                  </h2>
-                  <p>
-                    {content.text}
-                  </p>
-                </div>
-              ))}
-            </div>
-          ))}
-        </Slider>
+function ServiceCarousel() {
+  return (
+    <div>
+      <h1 id="service-title-h1">CONHEÇA OS NOSSOS SERVIÇOS</h1>
+      <div id="services">
+        {servicesArray.map((service) => (
+          <div className="service-card" key={service.name}>
+            <img
+              src={service.icon}
+              alt={service.name}
+              className="service-icon"
+            />
+            {service.content.map((content) => (
+              <div key={content.title} className="service-content-div">
+                <h2>{content.title}</h2>
+                <p>{content.text}</p>
+              </div>
+            ))}
+          </div>
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default ServiceCarousel;
